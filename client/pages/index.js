415ab5cc01e016610ce85abbbb4105b00515cf11
chrisbudi <chris_budi@outlook.com>
2022-03-19 02:08:13 +0700

    common

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,22 +2,17 @@ import axios from "axios";
 import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-    console.log(currentUser, "currentUser");
-    return currentUser ? <h1>You are signed in</h1> : <h1>You are not signed in</h1>;
+    return currentUser ?
+        <h1>You are signed in</h1> :
+        <h1>You are not signed in</h1>;
 };
 
 
 LandingPage.getInitialProps = async (context) => {
+    console.log("LANDING PAGE");
     const client = buildClient(context);
     const { data } = await client.get('api/users/currentuser');
     return data;
-
-    // const response = await axios.get('/api/users/currentUser').catch((err) => {
-    //     console.log(err.message, "error message");
-    // });
-    // console.log(response);
-
-    // return response.data;
 };
 
 export default LandingPage;
